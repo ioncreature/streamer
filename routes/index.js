d504c56beade6d1c7a1bd71096bae9b54deaa031
ioncreature <great_muchacho@mail.ru>
2016-06-16 01:07:49 +0300

    added mock actions

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
 var router = require( 'express' ).Router(),
-    fs = require( 'fs' );
+    fs = require( 'fs' ),
+    state = {};
+
+module.exports = router;
+
 
 router.get( '/', function ( req, res, next ){
     fs.readdir( './content', function( error, files ){
         if ( error )
             return next( error );
 
-        res.render( 'index', {files: files} );
+        res.render( 'index', {files: files.map(f => ({name: f, state: state[f]}))} );
     });
 });
 
-module.exports = router;
+
+router.get( '/play/:id', function( req, res ){
+    state[req.params.id] = 'playing';
+    res.redirect( '/' );
+});
+
+
+router.get( '/stop/:id', function( req, res ){
+    state[req.params.id] = 'stopped';
+    res.redirect( '/' );
+});
+
+
+router.get( '/stream/:id', function( req, res ){
+    res.render( 'stream', {name: req.params.id} );
+});
